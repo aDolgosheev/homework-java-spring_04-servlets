e0facb938a5730dfe65e8979509453df92bb032f
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-10-30 14:11:36 +0300

    di-annotation branch added - 2023.10.30

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,45 +1,44 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
+@Repository
 public class PostRepository {
-  private final ConcurrentMap<Long, Post> allPosts;
-  private final AtomicLong idCounter = new AtomicLong();
+    private final ConcurrentMap<Long, Post> allPosts;
+    private final AtomicLong idCounter = new AtomicLong();
 
-  public PostRepository() {
-    this.allPosts = new ConcurrentHashMap<>();
-  }
-
-  public Collection<Post> all() {
-    return allPosts.values();
-  }
-
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(allPosts.get(id));
-  }
-
-  public Post save(Post savePost) {
-    if (savePost.getId() == 0) {
-      long id = idCounter.incrementAndGet();
-      savePost.setId(id);
-      allPosts.put(id,savePost);
-    } else if (savePost.getId() != 0) {
-      Long currentId = savePost.getId();
-      allPosts.put(currentId, savePost);
+    public PostRepository() {
+        this.allPosts = new ConcurrentHashMap<>();
     }
-    return savePost;
-  }
 
-  public void removeById(long id) {
-    allPosts.remove(id);
-  }
+    public Collection<Post> all() {
+        return allPosts.values();
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(allPosts.get(id));
+    }
+
+    public Post save(Post savePost) {
+        if (savePost.getId() == 0) {
+            long id = idCounter.incrementAndGet();
+            savePost.setId(id);
+            allPosts.put(id,savePost);
+        } else if (savePost.getId() != 0) {
+            Long currentId = savePost.getId();
+            allPosts.put(currentId, savePost);
+        }
+        return savePost;
+    }
+
+    public void removeById(long id) {
+        allPosts.remove(id);
+    }
 }
